2a282a20c7104918f38cec57b6287d8fa74dc5ff
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-04-26 13:02:40 +0530

    initial commit

--- Algotithm_Problem/Algotithm_Problem/Program.cs
+++ Algotithm_Problem/Algotithm_Problem/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace AlgorithmProblem
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Problem");
+            Anagrams.FindAnagram();
+
+        }
+    }
+}
